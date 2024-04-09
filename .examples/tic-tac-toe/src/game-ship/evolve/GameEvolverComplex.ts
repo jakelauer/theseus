f352cc8790c1f46ccf935f84c0f5b6702ee1ac17
Jake Lauer <jakelauer@Jake-MacBook-M2-2.local>
2024-04-09 12:07:40 -0700

    adding missing evolvers to GameEvolverComplex

--- .examples/tic-tac-toe/src/game-ship/evolve/GameEvolverComplex.ts
+++ .examples/tic-tac-toe/src/game-ship/evolve/GameEvolverComplex.ts
@@ -2,8 +2,12 @@ import { EvolverComplex } from "theseus-js";
 import { GameState } from "../state/GameState";
 import { GamePlayEvolver } from "./evolvers/GamePlayEvolver";
 import { GameTurnEvolver } from "./evolvers/GameTurnEvolver";
+import { GameBoardEvolver } from "./evolvers/GameBoardEvolver";
+import { GameMetaEvolver } from "./evolvers/GameMetaEvolver";
 
 export const GameEvolverComplex = EvolverComplex.create<GameState>().withEvolvers({
     GamePlayEvolver,
     GameTurnEvolver,
+    GameBoardEvolver,
+    GameMetaEvolver,
 });
